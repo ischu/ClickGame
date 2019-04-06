e0f9290585c44ccd85f994559944adc38a9e238a
ianschu <ianschu7@gmail.com>
2019-04-06 15:07:01 -0400

    Updates

---
+++ precache-manifest.667981b8d0fe7c5ee83a5e07ab90ad4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d76327d9eefc62a77850",
+    "url": "/ClickGame/static/js/runtime~main.979c4616.js"
+  },
+  {
+    "revision": "e3786c476a8b2a4af925",
+    "url": "/ClickGame/static/js/main.5e4de026.chunk.js"
+  },
+  {
+    "revision": "63be4b4e181af0cf20d3",
+    "url": "/ClickGame/static/js/2.38d62158.chunk.js"
+  },
+  {
+    "revision": "e3786c476a8b2a4af925",
+    "url": "/ClickGame/static/css/main.0e43ca1b.chunk.css"
+  },
+  {
+    "revision": "63be4b4e181af0cf20d3",
+    "url": "/ClickGame/static/css/2.f8417c1b.chunk.css"
+  },
+  {
+    "revision": "5909a900213a4852238d7c931f7a6409",
+    "url": "/ClickGame/index.html"
+  }
+];
